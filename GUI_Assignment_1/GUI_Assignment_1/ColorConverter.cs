10c8b5334c08dd1f9e0f70c21320ad205739a888
Lena Todnem <lena.todnem@gmail.com>
2020-03-07 00:16:25 +0100

    Not done code - still needs to color red

---
+++ GUI_Assignment_1/GUI_Assignment_1/ColorConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Diagnostics;
+using GUI_Assignment_1;
+
+namespace GUI_Assignment_1
+{
+    public class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.Assert(targetType == typeof(Brush));
+            string money = value as string;
+            int moneyy=int.Parse(money);
+
+            return (moneyy <= 0 ? Brushes.Red : Brushes.Black);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
